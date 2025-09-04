94ea7400d1cee687e1d624e375efad5435b998d2
Ariyan Rahman Anas <anas.hllw@gmail.com>
2025-09-04 23:03:23 +0600

    project started and finished initial setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router'
+import { lazy } from 'react'
+
+const MainLayout = lazy(() => import("./components/layout/MainLayout"))
+const HomePage = lazy(() => import("./pages/HomePage"))
+const LoginPage = lazy(() => import("./pages/LoginPage"))
+const RegistrationPage = lazy(() => import("./pages/RegistrationPage"))
+const AboutPage = lazy(() => import("./pages/AboutPage"))
+const AdminLayout = lazy(() => import("./components/layout/AdminLayout"))
+
+
+const App = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      { path: "/", Component: HomePage },
+      { path: "/login", Component: LoginPage },
+      { path: "/registration", Component: RegistrationPage },
+      { path: "/", Component: HomePage },
+      { path: "/about", Component: AboutPage }
+    ]
+  },
+  {
+    path: "/admin",
+    Component: AdminLayout
+  }
+])
+export default App
